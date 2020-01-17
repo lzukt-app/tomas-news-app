2f403454d01d0f2bf44c8e10e3da7a42986219ab
tomasnei <tomas.sneideris@lzukt.lt>
2020-01-17 22:40:06 +0200

    News list style, Fix failing DateFormat

--- app/src/main/java/com/example/tomas_news_app/utils/ReFormatDate.kt
+++ app/src/main/java/com/example/tomas_news_app/utils/ReFormatDate.kt
@@ -4,11 +4,12 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 fun reFormatDate(dateTime: String): String {
-    val parseDateFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
+    val parseDateFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US)
     val displayDateFormatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
-    val date = parseDateFormatter.parse(dateTime)
+    val date = parseDateFormatter.parse(dateTime.substring(1, 19))
     return displayDateFormatter.format(date)
 }
+
 
 fun formatDate(date: Date): String {
     return SimpleDateFormat("yyyy-MM-dd", Locale.US).format(date)
