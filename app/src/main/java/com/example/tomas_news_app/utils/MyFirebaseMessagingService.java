998324048d21145c4bba8393e79aa3b644586d8d
tomasnei <tomas.sneideris@lzukt.lt>
2020-01-28 11:36:48 +0200

    Add Firebase

---
+++ app/src/main/java/com/example/tomas_news_app/utils/MyFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package com.example.tomas_news_app.utils;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
